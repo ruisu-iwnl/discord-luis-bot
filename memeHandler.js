e332f0fcfb584cc947675ec71863e7e7904884c3
Emanuel Luis Pelaez <35931283-eluispelaez@users.noreply.replit.com>
2024-08-23 19:23:15 +0000

    minor meme change

--- memeHandler.js
+++ memeHandler.js
@@ -1,8 +1,7 @@
-// memeHandler.js
 const axios = require("axios");
 
 const memeApiUrl = "https://meme-api.com/gimme";
-
+const allowedChannelId = "1276620142238765188"; 
 const fetchRandomMeme = async () => {
   try {
     const response = await axios.get(memeApiUrl);
@@ -15,11 +14,15 @@ const fetchRandomMeme = async () => {
 
 const handleMemeCommand = async (message) => {
   if (message.content.toLowerCase() === "meme") {
-    const memeUrl = await fetchRandomMeme();
-    if (memeUrl) {
-      message.channel.send(memeUrl);
+    if (message.channel.id === allowedChannelId) {
+      const memeUrl = await fetchRandomMeme();
+      if (memeUrl) {
+        message.channel.send(memeUrl);
+      } else {
+        message.channel.send("Sorry, I couldn't fetch a meme at the moment.");
+      }
     } else {
-      message.channel.send("Sorry, I couldn't fetch a meme at the moment.");
+      message.channel.send("bawal d2. punta k meme-bot channel/");
     }
   }
 };
