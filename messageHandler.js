52a36248bb232005ebc7d3529b42f0250e1c3fb7
Emanuel Luis Pelaez <35931283-eluispelaez@users.noreply.replit.com>
2024-08-23 10:42:05 +0000

    smol update

--- messageHandler.js
+++ messageHandler.js
@@ -1,7 +1,7 @@
 const { AttachmentBuilder } = require("discord.js");
 
-const monitoredChannelId = '1275732476408500255';
-const adminRoleId = '1275712561282547763';
+const monitoredChannelId = "1275732476408500255";
+const adminRoleId = "1275712561282547763";
 
 const handleMessages = async (message) => {
   if (message.author.bot) return;
@@ -11,69 +11,85 @@ const handleMessages = async (message) => {
     const hasAdminRole = message.member.roles.cache.has(adminRoleId);
 
     if (message.attachments.size === 0 && !hasAdminRole) {
-      message.delete()
-        .then(() => console.log(`Deleted message from channel ${monitoredChannelId}`))
+      message
+        .delete()
+        .then(() =>
+          console.log(`Deleted message from channel ${monitoredChannelId}`),
+        )
         .catch(console.error);
     }
   }
 
   // COMMANDS HANDLER
   if (/help po/i.test(message.content)) {
-    message.channel.send("```" +
-      "\nHello! I am bot luis. Ginawa ako para manggago. Maraming Salamat! :D" +
-      "\nhttps://github.com/ruisu-iwnl"+
-      "\n----------------------------------------------------" +
-      "\nFunny Commands:\n\nchino - Displays magic word\nzombie - Displays summoning word" +
-      "\n----------------------------------------------------" +
-      "\n!!!USEFUL Commands:\n\nhelp po - Displays this message'\npakilinis po - clears the last 10 messages" +
-      "\n----------------------------------------------------" +
-      "\n```"
+    message.channel.send(
+      "```" +
+        "\nHello! I am bot luis. Ginawa ako para manggago. Maraming Salamat! :D" +
+        "\nhttps://github.com/ruisu-iwnl" +
+        "\n----------------------------------------------------" +
+        "\nFunny Commands:\n\nchino - Displays magic word\nzombie - Displays summoning word" +
+        "\n----------------------------------------------------" +
+        "\n!!!USEFUL Commands:\n\nhelp po - Displays this message'\npakilinis po - clears the last 10 messages" +
+        "\n----------------------------------------------------" +
+        "\n```",
     );
   }
 
   if (/chino/i.test(message.content)) {
-    const attachment = new AttachmentBuilder('emote.jpg');
+    const attachment = new AttachmentBuilder("emote.jpg");
     message.channel.send({ content: "TANG INA MO!", files: [attachment] });
   }
 
   if (/zombie/i.test(message.content)) {
-    message.channel.send("```" +
-      "\nleft4dead reference? dying light?" +
-      "\n```"
+    message.channel.send(
+      "```" + "\nleft4dead reference? dying light?" + "\n```",
     );
   }
 
   if (/kuya/i.test(message.content)) {
-    const attachment = new AttachmentBuilder('boomer.png');
+    const attachment = new AttachmentBuilder("boomer.png");
     message.channel.send({
-      content: "```" +
-        "\nmabini homesite malapit sa dona adela (:" +
-        "\n```", 
-      files: [attachment]
+      content: "```" + "\nmabini homesite malapit sa dona adela (:" + "\n```",
+      files: [attachment],
     });
   }
 
-  if (message.content === "pakilinis po") {
+  if (message.content === "linis po") {
     const ask = async (q, filter) => {
       const tempMessage = await message.channel.send(`\`\`\`\n${q}\n\`\`\``);
-      const collected = await message.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] });
+      const collected = await message.channel.awaitMessages({
+        filter,
+        max: 1,
+        time: 30000,
+        errors: ["time"],
+      });
       await tempMessage.delete();
       return collected.first().content;
     };
 
-    const filterByUser = m => m.author.id === message.author.id;
+    const filterByUser = (m) => m.author.id === message.author.id;
 
-    let deleteCount = await ask("Ilan ba gusto mo burahin? (Max: 99)", m => !isNaN(m.content) && filterByUser(m));
-    deleteCount = Math.min(parseInt(deleteCount), 99); 
+    let deleteCount = await ask(
+      "Ilan ba gusto mo burahin? (Max: 99)",
+      (m) => !isNaN(m.content) && filterByUser(m),
+    );
+    deleteCount = Math.min(parseInt(deleteCount), 99);
 
-    const confirmation = await ask(`sure ka buburahin mo ${deleteCount} na mensahe? \n 1 : yes \n 2 : no`, m => ['1', '2'].includes(m.content) && filterByUser(m));
+    const confirmation = await ask(
+      `sure ka buburahin mo ${deleteCount} na mensahe? \n 1 : yes \n 2 : no`,
+      (m) => ["1", "2"].includes(m.content) && filterByUser(m),
+    );
 
-    if (confirmation === '1') {
-      const fetchedMessages = await message.channel.messages.fetch({ limit: deleteCount + 1 });
+    if (confirmation === "1") {
+      const fetchedMessages = await message.channel.messages.fetch({
+        limit: deleteCount + 1,
+      });
       await message.channel.bulkDelete(fetchedMessages, true);
       console.log(`Deleted the last ${deleteCount} messages.`);
     } else {
-      const tempMessage = await message.channel.send("walang binura na message.");
+      const tempMessage = await message.channel.send(
+        "walang binura na message.",
+      );
       setTimeout(() => tempMessage.delete(), 5000);
     }
   }
